fd4fcb3d417014c66a170344b9b55e26dc3a248b
Administrator <siddhiseth3009@gmail.com>
2020-09-28 16:14:47 +0530

    [Siddhi]Refactor. Employee Type based Daily wage computation added

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -15,14 +15,19 @@ namespace EmployeeWageComputation
         }
         static int EmployeeDailyWageComputation()
         {
-            int IS_PRESENT = 1;
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRESENT)
+            int empType = random.Next(0, 3);
+            if (empType == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if(empType == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
