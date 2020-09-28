cc28e39fca0114f09c035311ae72201eb15392d7
Administrator <siddhiseth3009@gmail.com>
2020-09-28 17:43:23 +0530

    [Siddhi]Refactor Employee Monthly Wage Calculated

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -12,31 +12,40 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            Console.WriteLine("Employee wage: " + EmployeeDailyWageComputation());
+            Console.WriteLine("Employee wage: " + EmployeeMonthlyWageComputation());
         }
 
         public const int EMP_RATE_PER_HOUR = 20;
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-
-        static int EmployeeDailyWageComputation()
+        public const int NUM_OF_WORKING_DAYS = 20;
+        static int EmployeeMonthlyWageComputation()
         {
             int empHrs = 0;
+            int empWage = 0;
+            int empCheck = 0;
+            int totalWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalWage += empWage;
             }
-            return empHrs * EMP_RATE_PER_HOUR;
+
+            return totalWage;
         }
     }
 }
